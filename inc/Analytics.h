43902996fd7166a33f1f4f003c02b4ed7640c1f4
John Hardin <john@tellient.com>
2014-12-16 18:13:56 -0800

    Initial commit.
    Mostly formatting changes and copyright updates, but also
    converted interface discovery to use the core About service.
    
    Change-Id: I2b1ff23ec94423d305df359f2f3acc39edbb0863
    Signed-off-by: John Hardin <john@tellient.com>

---
+++ inc/Analytics.h
@@ -0,0 +1,205 @@
+/******************************************************************************
+ *
+ *
+ * Copyright (c) 2014-2015, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+
+#ifndef ANALYTICS_H
+#define ANALYTICS_H
+
+#include <qcc/Debug.h>
+#include <qcc/String.h>
+
+#include <alljoyn/BusAttachment.h>
+#include <alljoyn/DBusStd.h>
+#include <alljoyn/AllJoynStd.h>
+#include <alljoyn/BusObject.h>
+#include <alljoyn/MsgArg.h>
+#include <alljoyn/version.h>
+#include <map>
+
+#define QCC_MODULE "ALLJOYN_ANALYTICS_SERVICE"
+
+
+/* Pure virtual interface to be implemented by analytics vendor. */
+class AnalyticsDeviceObject {
+    public:
+        /* public methods of the analytics interface, to be implemented. */
+
+        virtual QStatus SubmitEvent(const char **errMsg, const char *name,
+                size_t count, const ajn::MsgArg *kvs, uint64_t timestamp = 0) = 0;
+        virtual QStatus SetVendorData(const char **errMsg, size_t count,
+                const ajn::MsgArg *) = 0;
+        virtual QStatus SetDeviceData(const char **errMsg, size_t count,
+                const ajn::MsgArg *) = 0;
+        virtual void RequestDelivery() {}
+
+        /*
+         * called by the bus object when no more method calls are expected.
+         * the AnalyticsDeviceObject is expected to flush any buffered data
+         * to the cloud service and free itself.  After calling this,
+         * the AnalyticsBusObject is no longer responsible for freeing this
+         * object.
+         */
+        virtual void Shutdown() {
+            RequestDelivery();
+            delete this;
+        }
+
+        virtual ~AnalyticsDeviceObject() {};
+
+        /*
+         * An AnalyticsDeviceObject::Factory is passed to the constructor of
+         * the bus object to tell it how to make the appropriate
+         * AnalyticsDeviceObject.  The bus object will use this factory to
+         * constuct one device object for each client device connecting to
+         * the interface.
+         */
+
+        class Factory {
+            public:
+                virtual AnalyticsDeviceObject *Construct() = 0;
+                virtual void Destroy(AnalyticsDeviceObject *ado) { delete ado; }
+                virtual ~Factory() {}
+        };
+};
+
+
+class AnalyticsBusObject : public ajn::BusObject, ajn::BusListener {
+
+    public:
+
+        /*
+         * Utility method to Construct and install the appropriate
+         * InterfaceDescription for the analytics interface.  Useful
+         * in both a client and a service implementor.
+         */
+
+        static QStatus CreateInterface(ajn::BusAttachment &bus, const char *ifname)
+        {
+            using namespace ajn;
+            QStatus status = ER_OK;
+            InterfaceDescription *iface = NULL;
+            if (!bus.GetInterface(ifname)) {
+                status = bus.CreateInterface(ifname, iface, false);
+                if (status != ER_OK) {
+                    return status;
+                }
+                if (!iface) {
+                    return ER_BUS_CANNOT_ADD_INTERFACE;
+                }
+
+                status = iface->AddMethod("SetVendorData", "a{sv}", NULL, "values", 0);
+                if (status != ER_OK) {
+                    return status;
+                }
+                status = iface->AddMethod("SetDeviceData", "a{sv}", NULL, "values", 0);
+                if (status != ER_OK) {
+                    return status;
+                }
+                status = iface->AddMethod("RequestDelivery", NULL,NULL, "", 0);
+                if (status != ER_OK) {
+                    return status;
+                }
+
+                status = iface->AddMethod("SubmitEvent","stua{sv}",NULL, "name,timestamp,sequence,values", 0);
+                if (status != ER_OK) {
+                    return status;
+                }
+                iface->Activate();
+            }
+            return ER_OK;
+        }
+
+
+        AnalyticsBusObject(ajn::BusAttachment &bus, AnalyticsDeviceObject::Factory *factory, const char *path, const char *ifname) :
+            BusObject(path),
+            factory(factory),
+            bus(bus),
+            ifName(ifname)
+        {
+        }
+
+        virtual ~AnalyticsBusObject();
+
+        QStatus Initialize()
+        {
+            QStatus status = ER_OK;
+            QCC_DbgTrace(("AnalyticsService::%s", __FUNCTION__));
+
+            CreateInterface(bus, ifName.c_str());
+            const ajn::InterfaceDescription* intf = bus.GetInterface(ifName.c_str());
+            assert(intf);
+
+            status = AddInterface(*intf, ANNOUNCED);
+            if (status != ER_OK) {
+                return status;
+            }
+            const ajn::BusObject::MethodEntry methodEntries[] = {
+                { intf->GetMember("SubmitEvent"),
+                    static_cast<ajn::MessageReceiver::MethodHandler>(
+                            &AnalyticsBusObject::SubmitEvent)
+                },
+                { intf->GetMember("RequestDelivery"),
+                    static_cast<ajn::MessageReceiver::MethodHandler>(
+                            &AnalyticsBusObject::RequestDelivery)
+                },
+                { intf->GetMember("SetDeviceData"),
+                    static_cast<ajn::MessageReceiver::MethodHandler>(
+                            &AnalyticsBusObject::SetVendorDataOrDeviceData)
+                },
+                { intf->GetMember("SetVendorData"),
+                    static_cast<ajn::MessageReceiver::MethodHandler>(
+                            &AnalyticsBusObject::SetVendorDataOrDeviceData)
+                },
+            };
+            status = AddMethodHandlers(methodEntries, sizeof(methodEntries)/sizeof(ajn::BusObject::MethodEntry));
+            return status;
+        }
+
+        /* Used to detect session disconnect and handle clean up */
+        void NameOwnerChanged(const char *, const char *, const char *);
+
+    private:
+
+        /*
+         * method to supply vendor-specific data (api keys, etc)
+         * method to supply device identification data
+         */
+        void SetVendorDataOrDeviceData(const ajn::InterfaceDescription::Member*, ajn::Message &msg);
+
+        /* method to request immediate delivery to analytics vendor. */
+        void RequestDelivery(const ajn::InterfaceDescription::Member*, ajn::Message &msg);
+
+        /* method to log an analytics event. */
+        void SubmitEvent(const ajn::InterfaceDescription::Member*, ajn::Message &msg);
+
+        /*
+         * internal method to look up the object based on the bus name of the
+         * device that called this method, or Construct one if needed.
+         */
+        AnalyticsDeviceObject *MakeOrFindDev(ajn::Message &msg);
+
+        AnalyticsDeviceObject::Factory *factory;
+
+        std::map<std::string,AnalyticsDeviceObject *> devMap;
+
+        ajn::BusAttachment &bus;
+
+        qcc::String ifName;
+
+};
+
+#endif
---
+++ samples/sample_service.cc
@@ -0,0 +1,231 @@
+/**
+ * @file
+ * @brief Sample implementation of Tellient analytics service
+ */
+
+/******************************************************************************
+ * Copyright (c) 2014-2015, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+#include <signal.h>
+#include <stdio.h>
+#include <vector>
+
+#include <qcc/platform.h>
+
+#include <alljoyn/BusAttachment.h>
+#include <alljoyn/BusObject.h>
+#include <alljoyn/AboutObj.h>
+#include "TellientAnalytics.h"
+
+using namespace std;
+using namespace qcc;
+using namespace ajn;
+
+/*constants*/
+static const char* INTERFACE_NAME = "org.alljoyn.AnalyticsEventAgent";
+
+/*
+ * The service path may be used to help distinguish between multiple
+ * instances of services advertising the Analytics interface. A trusted
+ * relationship between client and service can be created by adding some
+ * type of shared secret on the interface. That is not included in this example.
+ */
+static const char* SERVICE_PATH = "/analytics/example";
+
+/* The app id must is a UUID. Generate a new one for your own analytics service implementation. */
+static const uint8_t MY_APP_ID[] = {
+    0x01, 0x8e, 0xc2, 0x19,
+    0x52, 0x9d, 0x49, 0x75,
+    0xbd, 0x6b, 0xe7, 0x06,
+    0x88, 0x5a, 0x4e, 0x80
+};
+
+/* Likewise, this should be a unique identifier for the device running the service*/
+static const qcc::String MY_DEVICE_ID = "this device id";
+
+/* About assigned service port for About service */
+static const ajn::SessionPort ASSIGNED_SERVICE_PORT = 900;
+
+static const qcc::String DEFAULT_LANGUAGE = "en";
+
+static volatile sig_atomic_t s_interrupt = false;
+
+static void SigIntHandler(int sig)
+{
+    s_interrupt = true;
+}
+
+class MySessionPortListener : public SessionPortListener {
+    public:
+        bool AcceptSessionJoiner(ajn::SessionPort sessionPort, const char* joiner, const ajn::SessionOpts& opts)
+        {
+            if (sessionPort != ASSIGNED_SERVICE_PORT) {
+                printf("Rejecting join attempt on unexpected session port %d.\n", sessionPort);
+                return false;
+            }
+            printf("Accepting join session request from %s (opts.proximity=%x, opts.traffic=%x, opts.transports=%x).\n",
+                    joiner, opts.proximity, opts.traffic, opts.transports);
+            return true;
+        }
+        void SessionJoined(SessionPort sessionPort, SessionId id, const char* joiner)
+        {
+            printf("Session Joined SessionId = %u\n", id);
+        }
+};
+
+static QStatus FillAboutPropertyStoreImplData(AboutData& aboutData)
+{
+    QStatus status = ER_OK;
+    status = aboutData.SetDeviceId(MY_DEVICE_ID.c_str());
+    if (status != ER_OK) {
+        return status;
+    }
+    status = aboutData.SetAppId(MY_APP_ID, 16);
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetAppName("Analytics service example");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetModelNumber("Wxfy388i");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetDateOfManufacture("2199-10-01");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetSoftwareVersion("12.20.44 build 44454");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetHardwareVersion("355.499. b");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetDeviceName("My device name", "en");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetDescription("This is an Alljoyn Application");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetManufacturer("Company");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    status = aboutData.SetSupportUrl("http://www.alljoyn.org");
+    if (status != ER_OK) {
+        return status;
+    }
+
+    return status;
+}
+
+/** Wait for SIGINT before continuing. */
+void WaitForSigInt(void)
+{
+    while (s_interrupt == false) {
+#ifdef _WIN32
+        Sleep(100);
+#else
+        usleep(100 * 1000);
+#endif
+    }
+}
+
+int main(int argc, char**argv, char**envArg)
+{
+    QStatus status = ER_OK;
+
+    /* Install SIGINT handler so Ctrl + C deallocates memory properly */
+    signal(SIGINT, SigIntHandler);
+
+    printf("AllJoyn Library version: %s.\n", ajn::GetVersion());
+    printf("AllJoyn Library build info: %s.\n", ajn::GetBuildInfo());
+
+    BusAttachment bus("Analytics Service Example", true);
+
+    status = bus.Start();
+    if (ER_OK != status) {
+        printf("Start of BusAttachment failed (%s).\n", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+
+    status = bus.Connect();
+    if (ER_OK != status) {
+        printf("Failed to connect daemon (%s)\n", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+
+    MySessionPortListener sessionPortListener;
+    SessionOpts opts(SessionOpts::TRAFFIC_MESSAGES, false, SessionOpts::PROXIMITY_ANY, TRANSPORT_ANY);
+    SessionPort sp = ASSIGNED_SERVICE_PORT;
+    status = bus.BindSessionPort(sp, opts, sessionPortListener);
+    if (ER_OK != status) {
+        printf("Failed to BindSessionPort (%s)", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+
+    AboutData aboutData("en");
+
+    status = FillAboutPropertyStoreImplData(aboutData);
+    if (ER_OK != status) {
+        printf("Failed to set aboutData (%s)\n", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+
+    TellientDevFactory devFactory;
+    AnalyticsBusObject testObj(bus, &devFactory, SERVICE_PATH, INTERFACE_NAME);
+
+    status = testObj.Initialize();
+    if (ER_OK != status) {
+        printf("Failed to initialize testObj (%s)\n", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+
+    status = bus.RegisterBusObject(testObj);
+    if (ER_OK != status) {
+        printf("Failed to register testObj (%s)\n", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+
+    AboutObj aboutObj(bus);
+
+    status = aboutObj.Announce(ASSIGNED_SERVICE_PORT, aboutData);
+    if (ER_OK != status) {
+        printf("aboutObj.Announce failed (%s)\n", QCC_StatusText(status));
+        return EXIT_FAILURE;
+    }
+    printf("up and running\n");
+
+    /* Perform the service asynchronously until the user signals for an exit. */
+    if (ER_OK == status) {
+        WaitForSigInt();
+    }
+
+    return 0;
+}
